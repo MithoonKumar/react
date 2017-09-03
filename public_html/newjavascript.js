026391875b76f244306d7ad906fe4675140499b3
Mithoon <Mithoon@192.168.0.101>
2017-09-03 16:30:04 +0530

    Added post request

--- public_html/newjavascript.js
+++ public_html/newjavascript.js
@@ -1,5 +1,16 @@
-window.onload=function(){
-  console.log("hello world");
+var fun=function(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+      if(this.readyState === 4 && this.status === 200)
+      {
+           document.getElementById("demo").innerHTML = this.responseText;
+           
+      }
+  };
+  xhttp.open("POST","http://192.168.0.103:3000/mithoon",true);
+  xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  var params = {name:"mithoon kumar"};
+  xhttp.send("sfsd=sds&dfgd=fd");
+     
     
-    
-}
+};
